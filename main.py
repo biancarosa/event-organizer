5c07999bada42bee967cb7ca4446fad6105b2346
Bianca Rosa <me@biancarosa.com.br>
2017-07-29 15:21:07 -0300

    funciona

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import mongoengine
+from models.event import Event
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+mongoengine.connect('organizer', username='bia', password='supersecurepwd')
+
+@app.route('/health-check')
+def health_check():
+    return jsonify({"message" : "I'm alive."})
+
+@app.route('/events', methods=['GET'])
+def list_events():
+    events_obj = Event.objects()
+    events = []
+    for event in events_obj:
+        events.append({'id' : str(event.id), 'name' : event.name, 'date' : event.date })
+    return jsonify({"data" : events}), 200
+
+@app.route('/events', methods=['POST'])
+def create_event():
+    data = request.get_json()
+    event = Event()
+    event.name = data.get('name')
+    event.date = data.get('date')
+    event.save()
+    return jsonify({"data" : {'id' : str(event.id), 'name' : event.name, 'date' : event.date }}), 201
---
+++ models/event.py
@@ -0,0 +1,7 @@
+from mongoengine import (
+    Document, StringField, DateTimeField
+)
+
+class Event(Document):
+    name = StringField(required=True)
+    date = DateTimeField()
